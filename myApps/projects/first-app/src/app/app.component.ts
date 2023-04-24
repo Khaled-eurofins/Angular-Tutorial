99f709a2b87527458e1d232a7e7b80e403373564
Khaled-eurofins <KhaledDamaj@eurofins.com>
2023-04-24 11:34:15 +0200

    My First angular app

---
+++ myApps/projects/first-app/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'FirstApp';
+  fontColor = 'blue';
+  sayHelloId = 1;
+  canClick = false;
+  message = 'Hello, World';
+  canEdit = false;
+
+  ngOnInit(): void {
+    console.log('OnInit');
+  }
+
+  sayMessage() {
+    alert(this.message);
+  }
+
+  onEditClick() {
+    this.canEdit = !this.canEdit;
+    if (this.canEdit) {
+      this.message = 'You can edit me!';
+    } else {
+      this.message = "I'm read only!";
+    }
+  }
+}
