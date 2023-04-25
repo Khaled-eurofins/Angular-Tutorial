e8b75c0bd44e7739f155c297207ac7e8c883bf64
Khaled-eurofins <KhaledDamaj@eurofins.com>
2023-04-25 14:38:23 +0200

    add routing

--- myApps/projects/employee-app/src/app/app-routing.module.ts
+++ myApps/projects/employee-app/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'employees', component: EmployeeListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
